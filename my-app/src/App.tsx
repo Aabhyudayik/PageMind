bb2cd06ebcf7971040e49eb2ba124ec490d5f451
Aabhyudayik <aabhyu.sharma@gmail.com>
2026-04-09 14:37:01 +0545

    Built the splashscreen and declared its route path along with the auth page

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,23 @@
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Splash from "./pages/splashscreen.tsx"
+import Auth from "./pages/auth"
 function App(){
   return(
-    <div>
-      <h1>PageMind</h1>
-    </div>
+   
+    <BrowserRouter>
+     
+    <Routes>
+      <Route path='/' element={<Splash />} />
+      <Route path='/auth' element={<Auth />} />
+      <Route path="*" element={<Splash />} />
+
+    </Routes>
+    </BrowserRouter>
+  
+   
   )
 }
 export default App
+
  
---
+++ my-app/src/pages/splashscreen.tsx
@@ -0,0 +1,22 @@
+import leaf from '../assets/leaf.png'
+import './splashscreen.css'
+function Splash(){
+
+    return(
+          <div className="splash-container">
+    <div className="splash-title-wrapper">
+      <img src={leaf} alt="leaf" className="splash-leaf" />
+      <h1 className="splash-title">PageMind</h1>
+    </div>
+    <p className="splash-motto">
+      <span style={{animationDelay: '0s'}}>Every</span>
+      <span style={{animationDelay: '1s'}}>page,</span>
+      <span style={{animationDelay: '2s'}}>a</span>
+      <span style={{animationDelay: '2.3s'}}>conversation</span>
+    </p>
+  </div>
+)
+   
+
+}
+export default Splash
